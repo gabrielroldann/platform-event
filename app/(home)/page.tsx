18fd0d88780c72d3a29a4af32c087614d0e8599b
Gabriel Roldan Tavares Crisostomo <gabriel.roldan@nonobyte.com.br>
2024-05-13 22:02:21 -0300

    feat: Update home page to display user session information

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,25 @@
-import Image from "next/image";
+"use server";
 
-export default function Home() {
-  return <div></div>;
+import { getServerSession } from "next-auth";
+import { authOptions } from "../_lib/auth";
+import LoginButton from "../_components/login-button";
+
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
+  console.log(session);
+
+  return (
+    <div className="px-5">
+      {session ? (
+        <div>
+          <h1>Welcome {session!.user!.email}</h1>
+        </div>
+      ) : (
+        <div className="mt-24 flex flex-col gap-2 w-full justify-center items-center">
+          <LoginButton />
+        </div>
+      )}
+    </div>
+  );
 }
