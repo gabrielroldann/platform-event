3f9ee125d8e211881e96c3b870dbfa52f5ed7679
Gabriel Roldan <roldanbiel@gmail.com>
2024-05-14 20:23:00 -0300

    feat: Update home page with login and event components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,26 +2,28 @@
 
 import { getServerSession } from "next-auth";
 import { authOptions } from "../_lib/auth";
-import LoginButton from "../_components/login-button";
-import { Select, SelectItem } from "../_components/ui/select";
+import { db } from "../_lib/prisma";
+import LoginPage from "../_components/login";
+import ShowEvents from "../_components/show-events";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
   const types = await db.typeUser.findMany();
+  const events = await db.event.findMany();
 
   console.log(session);
 
   return (
-    <div className="px-5">
+    <div className="p-5 m-0">
       {session ? (
         <div>
-          <h1>Welcome {session!.user!.name}</h1>
+          <h1>Olá, {session!.user!.name}</h1>
+          <ShowEvents events={events} />
         </div>
       ) : (
         <div className="mt-24 flex flex-col gap-2 w-full justify-center items-center">
-          <Select></Select>
-          <LoginButton />
+          <LoginPage types={types} />
         </div>
       )}
     </div>
