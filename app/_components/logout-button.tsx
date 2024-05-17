67c3f766be7be19f4675fa24339afc9378d504fd
Gabriel Roldan Tavares Crisostomo <gabriel.roldan@nonobyte.com.br>
2024-05-16 22:08:25 -0300

    refactor: Update LogoutButton component to include logout icon

--- app/_components/logout-button.tsx
+++ app/_components/logout-button.tsx
@@ -2,6 +2,7 @@
 
 import { signOut } from "next-auth/react";
 import { Button } from "./ui/button";
+import { LogOut } from "lucide-react";
 
 const LogoutButton = () => {
   const handleLogout = () => {
@@ -9,7 +10,13 @@ const LogoutButton = () => {
   };
   return (
     <div>
-      <Button onClick={handleLogout}>Logout</Button>
+      <Button
+        onClick={handleLogout}
+        className="text-xl font-normal px-8 py-6 flex gap-2 item-center"
+      >
+        <LogOut size={18} />
+        Sair
+      </Button>
     </div>
   );
 };
