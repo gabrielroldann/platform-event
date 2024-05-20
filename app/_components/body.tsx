e9c065a786c1a4999241017e33a2d11ebef91ca0
Gabriel Roldan <roldanbiel@gmail.com>
2024-05-19 21:09:26 -0300

    refactor: Add Body component for displaying events and search functionality

---
+++ app/_components/body.tsx
@@ -0,0 +1,20 @@
+"use server";
+
+import { db } from "../_lib/prisma";
+import Search from "./search";
+
+const Body = async () => {
+  const events = await db.event.findMany();
+  return (
+    <div>
+      <div>
+        <Search events={events} />
+      </div>
+      <div>
+        <h1 className="text-3xl font-medium">Destaques</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Body;
