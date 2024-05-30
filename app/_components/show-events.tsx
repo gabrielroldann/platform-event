e8232bada9c126d2d2b2b9e081e2868fff539fb0
Gabriel Roldan <roldanbiel@gmail.com>
2024-05-30 17:24:05 -0300

    refactor: Remove unused imports and update ShowEvents component layout

--- app/_components/show-events.tsx
+++ app/_components/show-events.tsx
@@ -1,8 +1,4 @@
-"use client";
-
 import { Event } from "@prisma/client";
-import EmptyEvents from "./empty-events";
-import { Card, CardContent, CardHeader } from "./ui/card";
 
 interface ShowEventsProps {
   events: Event[];
@@ -11,25 +7,7 @@ interface ShowEventsProps {
 const ShowEvents = ({ events }: ShowEventsProps) => {
   const listEvents = events.length;
 
-  return (
-    <div>
-      {listEvents > 0 ? (
-        <div className="flex gap-3">
-          {/* {events.map((event) => (
-            <Card key={event.id}>
-              <CardContent key={event.id}>
-                <p className="font-medium text-xl">{event.title}</p>
-                <p className="text-muted-foreground">{event.location}</p>
-              </CardContent>
-            </Card>
-          ))} */}
-          <h1>EVENTS HERE</h1>
-        </div>
-      ) : (
-        <EmptyEvents />
-      )}
-    </div>
-  );
+  return <div></div>;
 };
 
 export default ShowEvents;
