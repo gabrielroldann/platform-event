5667df880ac94e0058fa4ffacb5ff69b945b55aa
Gabriel Roldan <roldanbiel@gmail.com>
2024-06-03 23:21:59 -0300

    refactor: Update AllEventsPage component to include server-side session retrieval and authentication options

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,10 +1,16 @@
+"use server";
+
+import { getServerSession } from "next-auth";
 import FAQ from "../_components/faq";
 import Footer from "../_components/footer";
 import Header from "../_components/header";
 import Search from "../_components/search";
 import ShowAllEvents from "./_components/show-all";
+import { authOptions } from "../_lib/auth";
 
 const AllEventsPage = () => {
+  const session = getServerSession(authOptions);
+
   return (
     <div>
       <div className="p-5 px-8 m-0">
